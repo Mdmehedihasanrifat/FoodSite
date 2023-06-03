afb08f8dcdc1f76a473df3d7535ef49320f06c07
Mdmehedihasanrifat <mehedihasan199607@gmail.com>
2023-06-03 18:02:53 +0600

    navbar working

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 
-let menu=document.getElementById("menu-icon");
-let navbar=document.querySelector(".navbar");
-menu.addEventListener("click",function(){
-  
-    navbar.classList.toggle("active");
-   alert("alert");
-})
+const menuIcon = document.getElementById('menu-icon');
+const navbar = document.querySelector('.navbar');
+
+// Add a click event listener to the menu icon
+menuIcon.addEventListener('click', () => {
+  // Toggle the 'active' class on the navbar
+  navbar.classList.toggle('active');
+});
 
 window.onscroll=()=>{
     navbar.classList.remove("active");
